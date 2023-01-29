44f446f0814bfef0eb1018d16b9c40fb6bfaac95
Jonathan Cai <caimjonathan@gmail.com>
2023-01-29 01:09:25 -0500

    feat: middleware

---
+++ middleware.page.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all paths except for:
+//      * 1. /api routes
+//      * 2. /_next (Next.js internals)
+//      * 3. /examples (inside /public)
+//      * 4. all root files inside /public (e.g. /favicon.ico)
+//      */
+//     '/((?!api/|_next/|_static/|examples/|[\\w-]+\\.\\w+).*)',
+//   ],
+// };
+
+export default async function middleware(req: NextRequest) {
+  const url = req.nextUrl;
+  console.log('HIT MIDDLEWARE');
+  return NextResponse.rewrite(url);
+
+  // // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
+  // const hostname = req.headers.get('host') || 'demo.vercel.pub';
+
+  // // Get the pathname of the request (e.g. /, /about, /blog/first-post)
+  // const path = url.pathname;
+
+  // const currentHost =
+  //   process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
+  //     ? hostname.replace(`.tokengate.xyz`, '').replace(`.x-aave.vercel.app`, '')
+  //     : hostname.replace(`.localhost:3000`, '');
+
+  // console.log({ currentHost, hostname });
+
+  // // rewrite root application to `/` folder
+  // if (
+  //   hostname === 'localhost:3000' ||
+  //   hostname === 'x-aave.vercel.app' ||
+  //   hostname === 'tokengate.xyz' ||
+  //   hostname === 'www.tokengate.xyz'
+  // ) {
+  //   return NextResponse.rewrite(new URL(path, req.url));
+  // }
+
+  // // rewrite everything else to `/_sites/[site] dynamic route
+  // return NextResponse.rewrite(new URL(`/_sites/${currentHost}${path}`, req.url));
+}
--- pages/_sites/[site]/index.page.tsx
+++ pages/_sites/[site]/index.page.tsx
@@ -1,16 +1,15 @@
 import { Trans } from '@lingui/macro';
 import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { useEffect, useState } from 'react';
+import { ConnectWalletPaper } from 'src/components/ConnectWalletPaper';
+import { ContentContainer } from 'src/components/ContentContainer';
 import StyledToggleButton from 'src/components/StyledToggleButton';
 import StyledToggleButtonGroup from 'src/components/StyledToggleButtonGroup';
 import { usePermissions } from 'src/hooks/usePermissions';
-
-import { ConnectWalletPaper } from '../src/components/ConnectWalletPaper';
-import { ContentContainer } from '../src/components/ContentContainer';
-import { MainLayout } from '../src/layouts/MainLayout';
-import { useWeb3Context } from '../src/libs/hooks/useWeb3Context';
-import { DashboardContentWrapper } from '../src/modules/dashboard/DashboardContentWrapper';
-import { DashboardTopPanel } from '../src/modules/dashboard/DashboardTopPanel';
+import { MainLayout } from 'src/layouts/MainLayout';
+import { useWeb3Context } from 'src/libs/hooks/useWeb3Context';
+import { DashboardContentWrapper } from 'src/modules/dashboard/DashboardContentWrapper';
+import { DashboardTopPanel } from 'src/modules/dashboard/DashboardTopPanel';
 
 export default function Home() {
   const { breakpoints } = useTheme();
